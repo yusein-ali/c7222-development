233ee68c2cfb0c49ec07f51acddab4187667358d
Yusein Ali <yusein.ali@aalto.fi>
2026-02-05 14:40:07 +0200

    Refactor BLE configuration variable names for consistency in CMake files

--- libs/elec_c7222/elec_c7222.cmake
+++ libs/elec_c7222/elec_c7222.cmake
@@ -3,17 +3,18 @@ set(ELEC_C7222_DIR ${CMAKE_CURRENT_LIST_DIR})
 add_library(ELEC_C7222 INTERFACE)
 
 # Determine whether BLE support is enabled
-if (NOT DEFINED ELEC_C7222_ENABLE_BLE)
+if (NOT DEFINED C7222_ENABLE_BLE)
     if(DEFINED PICO_BT_ENABLE_BLE)
-        set(ELEC_C7222_ENABLE_BLE ${PICO_BT_ENABLE_BLE})
+        set(C7222_ENABLE_BLE ${PICO_BT_ENABLE_BLE})
     elseif(DEFINED ENABLE_BLE)
-        set(ELEC_C7222_ENABLE_BLE ${ENABLE_BLE})
-    elseif(NOT DEFINED ELEC_C7222_ENABLE_BLE)
-        option(ELEC_C7222_ENABLE_BLE "Enable BLE helpers in ELEC_C7222" OFF)
+        set(C7222_ENABLE_BLE ${ENABLE_BLE})
+    elseif(NOT DEFINED C7222_ENABLE_BLE)
+        option(C7222_ENABLE_BLE "Enable BLE helpers in ELEC_C7222" OFF)
     endif()
 endif()
 
-message(STATUS "ELEC_C7222_ENABLE_BLE is defined: ${ELEC_C7222_ENABLE_BLE}")
+message(STATUS "C7222_ENABLE_BLE is defined: ${C7222_ENABLE_BLE}")
+message(STATUS "C7222_ENABLE_BLE_DEBUG is defined: ${C7222_BLE_DEBUG}" )
 # Immediate child modules
 include(${ELEC_C7222_DIR}/devices/devices.cmake)
 include(${ELEC_C7222_DIR}/utils/utils.cmake)
@@ -26,7 +27,7 @@ target_link_libraries(ELEC_C7222 INTERFACE
 target_include_directories(ELEC_C7222 INTERFACE "${ELEC_C7222_DIR}/include")
 target_include_directories(ELEC_C7222 INTERFACE "${ELEC_C7222_DIR}/config")
 
-if(ELEC_C7222_ENABLE_BLE)
+if(C7222_ENABLE_BLE)
     include(${ELEC_C7222_DIR}/ble/ble.cmake)
     target_link_libraries(ELEC_C7222 INTERFACE ELEC_C7222_BLE)
 endif()
